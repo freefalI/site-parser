3a3feb24c7073fa5880d92734ca6dfa55d33c152
ashmatko <ashmatko758@gmail.com>
2020-01-20 00:24:33 +0200

    scrapper implementations

--- src/Scrappers/ImageScraper.php
+++ src/Scrappers/ImageScraper.php
@@ -9,7 +9,8 @@
 namespace SiteParser\Scrappers;
 
 use SiteParser\DTO\ScrapedData;
-use SiteParser\DTO\ScrapedImages;
+use SiteParser\DTO\ScrapedUrls;
+use SiteParser\ValueObjects\ImageUrl;
 use SiteParser\ValueObjects\Url;
 
 class ImageScraper extends WebPageScraper
@@ -22,8 +23,15 @@ class ImageScraper extends WebPageScraper
      */
     public function run(Url $url, string $html)
     {
-        // TODO: Implement run() method.
-
-        return new ScrapedImages($url, [new Url('789'), new Url('123')]);
+        $regex = '/(?:src=")(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’]))/';
+        if (preg_match_all($regex, $html, $matches)) {
+            print_r($matches[1]);
+        }
+        $results = [];
+        foreach ($matches[1] as $match) {
+            $results[] = new ImageUrl($match);
+        }
+        return new ScrapedUrls($url, $results);
+//        return new ScrapedImages($url, [new ImageUrl('789'), new ImageUrl('123')]);
     }
 }
--- src/Scrappers/UrlScraper.php
+++ src/Scrappers/UrlScraper.php
@@ -22,8 +22,15 @@ class UrlScraper extends WebPageScraper
      */
     public function run(Url $url, string $html)
     {
-        // TODO: Implement run() method.
-
-        return new ScrapedUrls($url, [new Url('123'), new Url(456)]);
+        $regex = '/(?:href=")(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’]))/';
+        if (preg_match_all($regex, $html, $matches)) {
+            print_r($matches[1]);
+        }
+        $results = [];
+        foreach ($matches[1] as $match) {
+            $results[] = new Url($match);
+        }
+//        return new ScrapedUrls($url, [new Url('123'), new Url(456)]);
+        return new ScrapedUrls($url, $results);
     }
 }
